b6518c01e84e81001e29ec4379bc9f64241c4feb
salma khattabi <salmakhattabi950@gmail.com>
2021-11-12 17:00:43 +0100

    home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,9 +22,11 @@ ActiveStorage.start()
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// import { card_carousel } from '../plugins/card_carousel';
+// import { hoverCard } from '../vanilla/hover_card';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  // hoverCard();
 });
