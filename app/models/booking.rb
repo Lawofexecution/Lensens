34138ba26fa4b7605fe20172c6eddb5bc6605b14
salma khattabi <salmakhattabi950@gmail.com>
2021-11-18 16:03:25 +0100

    form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -15,7 +15,7 @@
 class Booking < ApplicationRecord
   belongs_to :client, class_name: "User"
   belongs_to :creator, class_name: "User"
-  #validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, presence: true
   #validate :end_date_after_start_date
 
   private
