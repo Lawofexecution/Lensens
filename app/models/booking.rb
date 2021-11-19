0f774bd1d871997370e71547fb864dae634cc0da
Lawofexecution <90073954+Lawofexecution@users.noreply.github.com>
2021-11-19 12:25:50 +0100

    Delete unused files

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,17 +1,3 @@
-# class AvailabilityValidator < ActiveModel::EachValidator
-
-#   def validate_each(record, attribute, value)
-#     bookings = Booking.where(["creator_id =?", record.creator_id])
-#     date_ranges = bookings.map { |b| b.start_date..b.end_date }
-
-#     date_ranges.each do |range|
-#       if range.include? value
-#         record.errors.add(attribute, "not available")
-#       end
-#     end
-#   end
-# end
-
 class Booking < ApplicationRecord
   belongs_to :client, class_name: "User"
   belongs_to :creator, class_name: "User"
